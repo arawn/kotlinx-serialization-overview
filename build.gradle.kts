945226d30b5f96d29d20f2617a74f70342e7da0b
Arawn Park <arawn.kr@gmail.com>
2022-05-08 23:34:56 +0900

    Add simple gson example

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
+    implementation("com.google.code.gson:gson:2.9.0")
     testImplementation("io.kotest:kotest-runner-junit5:5.3.0")
     testImplementation("io.kotest:kotest-assertions-core:5.3.0")
 }
---
+++ src/test/kotlin/GsonSpec.kt
@@ -0,0 +1,37 @@
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class GsonSpec : FunSpec({
+
+    val gson = Gson()
+
+    test("객체를 JSON으로 직렬화 또는 역직렬화하기") {
+        val data = Movie("foo", "x", 0.1)
+
+        val serialized = gson.toJson(data)
+        serialized shouldBe """{"title":"foo","director":"x","rating":0.1}"""
+
+        // val deserialized = gson.fromJson(serialized, Movie::class.java)
+        // val deserialized = gson.fromJson<Movie>(serialized, object : TypeToken<Movie>() {}.type)
+        val deserialized = gson.fromJson<Movie>(serialized)
+        deserialized shouldBe data
+    }
+
+    test("리스트를 JSON으로 직렬화 또는 역직렬화하기") {
+        val data = listOf(Movie("foo", "x", 0.1), Movie("bar", "y", 9.9))
+
+        val serialized = gson.toJson(data)
+        serialized shouldBe """[{"title":"foo","director":"x","rating":0.1},{"title":"bar","director":"y","rating":9.9}]"""
+
+        // val javaType = object : TypeToken<List<Movie>>() {}.type
+        // val deserialized = gson.fromJson<List<Movie>>(serialized, javaType)
+        val deserialized = gson.fromJson<List<Movie>>(serialized)
+        deserialized shouldBe data
+    }
+})
+
+internal inline fun <reified T> Gson.fromJson(
+    jsonString: String
+): T = fromJson(jsonString, object : TypeToken<T>() {}.type)
